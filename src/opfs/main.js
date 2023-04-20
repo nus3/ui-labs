cf2c92b943efeb1524f070182906bcae7d2e8e96
nus3 <hadayota33@gmail.com>
2023-04-20 12:02:57 +0900

    add view directory tree

--- src/opfs/main.js
+++ src/opfs/main.js
@@ -50,61 +50,126 @@ const deleteDir = async (dirName) => {
   await dirHandle.remove({ recursive: true })
 }
 
-const moveToDir = async (dirName) => {
+const moveToDir = async (dirName, fileName) => {
   const dirHandle = await getDirHandle(dirName)
-  const fileHandle = await getFileHandle('nus1')
+  const fileHandle = await getFileHandle(fileName)
   await fileHandle.move(dirHandle)
 }
 
-const resolvePath = async (fileName) => {
+const resolvePath = async (dirName, fileName) => {
   const opfsRoot = await navigator.storage.getDirectory()
-  const fileHandle = await getFileHandle(fileName)
+  const dirHandle = await getDirHandle(dirName)
+  const fileHandle = await dirHandle.getFileHandle(fileName)
   console.log(await opfsRoot.resolve(fileHandle))
 }
 
-const main = () => {
+const getDirectoryEntriesRecursive = async (
+  directoryHandle,
+  relativePath = '.',
+) => {
+  const entries = {}
+  const directoryIterator = directoryHandle.values()
+  const directoryEntryPromises = []
+
+  for await (const handle of directoryIterator) {
+    const nestedPath = `${relativePath}/${handle.name}`
+    if (handle.kind === 'file') {
+      directoryEntryPromises.push(handle)
+    } else if (handle.kind === 'directory') {
+      directoryEntryPromises.push(
+        (async () => {
+          return {
+            name: handle.name,
+            kind: handle.kind,
+            relativePath: nestedPath,
+            entries: await getDirectoryEntriesRecursive(handle, nestedPath),
+            handle,
+          }
+        })(),
+      )
+    }
+  }
+  const directoryEntries = await Promise.all(directoryEntryPromises)
+  directoryEntries.forEach((directoryEntry) => {
+    entries[directoryEntry.name] = directoryEntry
+  })
+  return entries
+}
+
+const createList = (entries) => {
+  const ul = document.createElement('ul')
+
+  for (let key in entries) {
+    const entry = entries[key]
+    const li = document.createElement('li')
+    const label = entry.kind === 'directory' ? `dir: ${entry.name}` : entry.name
+    li.innerText = label
+    if (entry.kind === 'directory' && entry.entries) {
+      li.appendChild(createList(entry.entries))
+    }
+    ul.appendChild(li)
+  }
+
+  return ul
+}
+
+const list = async () => {
+  const opfsRoot = await navigator.storage.getDirectory()
+  const entries = await getDirectoryEntriesRecursive(opfsRoot)
+  console.log(entries)
+  return entries
+}
+
+const main = async () => {
+  const entries = await list()
+  const ul = createList(entries)
+  document.querySelector('#mListWrapper')?.appendChild(ul)
+
   const mCreate = document.querySelector('#mCreate')
-  mCreate.addEventListener('click', async () => {
-    create('nus1')
+  mCreate.addEventListener('click', () => {
+    create('nus3-1')
   })
 
   const mCreateDir = document.querySelector('#mCreateDir')
-  mCreateDir.addEventListener('click', async () => {
-    createDir('nus1-dir')
+  mCreateDir.addEventListener('click', () => {
+    createDir('nus3-dir')
   })
 
   const mRead = document.querySelector('#mRead')
-  mRead.addEventListener('click', async () => {
+  mRead.addEventListener('click', () => {
     read('nus1')
   })
 
   const mWrite = document.querySelector('#mWrite')
-  mWrite.addEventListener('click', async () => {
+  mWrite.addEventListener('click', () => {
     write('nus1')
   })
 
   const mDelete = document.querySelector('#mDelete')
-  mDelete.addEventListener('click', async () => {
+  mDelete.addEventListener('click', () => {
     deleteFile('nus1')
   })
 
   const mDeleteDir = document.querySelector('#mDeleteDir')
-  mDeleteDir.addEventListener('click', async () => {
+  mDeleteDir.addEventListener('click', () => {
     deleteDir('nus2-directory')
   })
 
   const mMove = document.querySelector('#mMove')
-  mMove.addEventListener('click', async () => {
-    moveToDir('nus1-dir')
+  mMove.addEventListener('click', () => {
+    moveToDir('nus3-dir', 'nus3-1')
   })
 
   const mResolvePath = document.querySelector('#mResolvePath')
-  mResolvePath.addEventListener('click', async () => {
-    resolvePath('nus1')
+  mResolvePath.addEventListener('click', () => {
+    resolvePath('nus1-dir', 'nus1')
   })
 
-  // TODO: ネストしたファイルの現在位置を表示する
-  // TODO: フォルダ一覧を表示する
+  const mList = document.querySelector('#mList')
+  mList.addEventListener('click', () => {
+    list()
+  })
+
   // TODO: webworkerを使って同期的にファイル操作する
 }
 
